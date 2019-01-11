df69ae2132a7ee35e8958eb06cf028254b4f60d7
Andrew Minnich <aminnich3@gmail.com>
2019-01-11 01:10:33 -0500

    hello iplug 2

---
+++ source/Flutterbird.cpp
@@ -0,0 +1,38 @@
+#include "Flutterbird.h"
+#include "IPlug_include_in_plug_src.h"
+#include "IControls.h"
+
+Flutterbird::Flutterbird(IPlugInstanceInfo instanceInfo)
+: IPLUG_CTOR(kNumParams, kNumPrograms, instanceInfo)
+{
+  GetParam(kGain)->InitDouble("Gain", 100., 0., 100.0, 0.01, "%");
+
+#if IPLUG_EDITOR // All UI methods and member variables should be within an IPLUG_EDITOR guard, should you want distributed UI
+  mMakeGraphicsFunc = [&]() {
+    return MakeGraphics(*this, PLUG_WIDTH, PLUG_HEIGHT, PLUG_FPS, 1.);
+  };
+  
+  mLayoutFunc = [&](IGraphics* pGraphics) {
+    pGraphics->AttachCornerResizer(kUIResizerScale, false);
+    pGraphics->AttachPanelBackground(COLOR_GRAY);
+    pGraphics->LoadFont(ROBOTTO_FN);
+    const IRECT b = pGraphics->GetBounds();
+    pGraphics->AttachControl(new ITextControl(*this, b, "Hello iPlug 2!", IText(50)));
+    pGraphics->AttachControl(new IVKnobControl(*this, b.GetCentredInside(100).GetVShifted(-100), kGain));
+  };
+#endif
+}
+
+#if IPLUG_DSP
+void Flutterbird::ProcessBlock(sample** inputs, sample** outputs, int nFrames)
+{
+  const double gain = GetParam(kGain)->Value() / 100.;
+  const int nChans = NOutChansConnected();
+  
+  for (int s = 0; s < nFrames; s++) {
+    for (int c = 0; c < nChans; c++) {
+      outputs[c][s] = inputs[c][s] * gain;
+    }
+  }
+}
+#endif
---
+++ source/projects/FlutterbirdFramework.h
@@ -0,0 +1,13 @@
+@import UIKit;
+
+//! Project version number for FlutterbirdFramework.
+FOUNDATION_EXPORT double FlutterbirdFramework_VersionNumber;
+
+//! Project version string for FlutterbirdFramework.
+FOUNDATION_EXPORT const unsigned char FlutterbirdFramework_VersionString[];
+
+#import <FlutterbirdFramework/config.h>
+#import <FlutterbirdFramework/IPlugAUAudioUnit.h>
+
+@class FlutterbirdViewController;
+
