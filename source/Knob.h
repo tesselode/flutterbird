4692686fd7c0e167420dc7ff22a8d224e7312782
Andrew Minnich <aminnich3@gmail.com>
2019-01-14 21:27:16 -0500

    knob class cleanup

--- source/Knob.h
+++ source/Knob.h
@@ -17,43 +17,27 @@ public:
 
 	void Draw(IGraphics &g) override
 	{
-		const float v = mAngleMin + ((float)mValue * (mAngleMax - mAngleMin));
 		const float cx = mRECT.MW(), cy = mRECT.MH();
+		const float angle = mAngleMin + ((float)mValue * (mAngleMax - mAngleMin));
 		const float radius = (mRECT.W() / 2.75f);
-		float originAngle;
-		switch (origin)
-		{
-		case KnobOrigin::Left:
-			originAngle = mAngleMin;
-			break;
-		case KnobOrigin::Center:
-			originAngle = mAngleMin + (mAngleMax - mAngleMin) / 2;
-			break;
-		case KnobOrigin::Right:
-			originAngle = mAngleMax;
-			break;
-		}
-		float arcAngleA = originAngle < v ? originAngle : v;
-		float arcAngleB = arcAngleA == originAngle ? v : originAngle;
+		float arcStartAngle, arcEndAngle;
+		GetArcStartAndEndAngles(angle, arcStartAngle, arcEndAngle);
+
 		g.DrawArc(themeColorDark, cx, cy, radius, mAngleMin, mAngleMax, 0, 12.f);
-		g.DrawArc(themeColorAccent, cx, cy, radius, arcAngleA, arcAngleB, 0, 12.f);
+		g.DrawArc(themeColorAccent, cx, cy, radius, arcStartAngle, arcEndAngle, 0, 12.f);
 		g.FillCircle(mMouseIsOver ? themeColorLight : themeColorWhite, cx, cy, radius);
-		g.DrawRadialLine(themeColorAccent, cx, cy, v, 0.5f * radius, .9f * radius, 0, 3.f);
-		if (mLabelBounds.H())
-			g.DrawText(mLabelText, mLabel.Get(), mLabelBounds);
-		if (mDisplayParamValue)
-		{
-			WDL_String str;
-			GetParam()->GetDisplayForHost(str);
-			if (mShowParamLabel)
-			{
-				str.Append(" ");
-				str.Append(GetParam()->GetLabelForHost());
-			}
-			g.DrawText(mValueText, str.Get(), mValueBounds);
-		}
+		g.DrawRadialLine(themeColorAccent, cx, cy, angle, 0.5f * radius, .9f * radius, 0, 3.f);
 	}
 
 private:
 	KnobOrigin origin;
+
+	void GetArcStartAndEndAngles(float knobAngle, float &start, float &end)
+	{
+		float originAngle = origin == KnobOrigin::Right ? mAngleMax :
+			origin == KnobOrigin::Center ? mAngleMin + (mAngleMax - mAngleMin) / 2:
+			mAngleMin;
+		start = originAngle < knobAngle ? originAngle : knobAngle;
+		end = start == originAngle ? knobAngle : originAngle;
+	}
 };
