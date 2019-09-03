e5c07ec951e58e1b95d3138484cb31a898445114
Andrew Minnich <aminnich3@gmail.com>
2019-09-03 00:25:33 -0400

    write samples to buffer + imgui debug display

--- source/Flutterbird.cpp
+++ source/Flutterbird.cpp
@@ -5,6 +5,10 @@
 Flutterbird::Flutterbird(const InstanceInfo& info)
 : Plugin(info, MakeConfig(0, 1))
 {
+#if IPLUG_DSP
+	InitTape();
+#endif
+
 #if IPLUG_EDITOR // All UI methods and member variables should be within an IPLUG_EDITOR guard, should you want distributed UI
 	mMakeGraphicsFunc = [&]()
 	{
@@ -15,19 +19,50 @@ Flutterbird::Flutterbird(const InstanceInfo& info)
 	{
 		pGraphics->AttachCornerResizer(EUIResizerMode::Scale, false);
 		pGraphics->AttachPanelBackground(COLOR_GRAY);
+		pGraphics->AttachImGui([this](IGraphics* pGraphics) {
+			ImGui::PlotLines("Tape L", &floatTapeL[0], std::size(tapeL), writePosition, 0, -1.0, 1.0, ImVec2(0, 100));
+			ImGui::PlotLines("Tape R", &floatTapeR[0], std::size(tapeR), writePosition, 0, -1.0, 1.0, ImVec2(0, 100));
+			ImGui::Text("Write position:");
+			ImGui::SameLine();
+			ImGui::Text(std::to_string(writePosition).c_str());
+		});
 	};
 #endif
 }
 
 #if IPLUG_DSP
+void Flutterbird::InitTape()
+{
+	tapeL.clear();
+	tapeR.clear();
+	floatTapeL.clear();
+	floatTapeR.clear();
+	auto numberOfSamples = tapeLengthInSeconds * GetSampleRate();
+	for (int i = 0; i < numberOfSamples; i++)
+	{
+		tapeL.push_back(0.0);
+		tapeR.push_back(0.0);
+		floatTapeL.push_back(0.0);
+		floatTapeR.push_back(0.0);
+	}
+	writePosition = 0;
+}
+
 void Flutterbird::ProcessBlock(sample** inputs, sample** outputs, int nFrames)
 {
-	for (int sample = 0; sample < nFrames; sample++)
+	for (int s = 0; s < nFrames; s++)
 	{
-		for (int channel = 0; channel < NOutChansConnected(); channel++)
-		{
-			outputs[channel][sample] = inputs[channel][sample];
-		}
+		auto inL = inputs[0][s];
+		auto inR = inputs[1][s];
+		tapeL[writePosition] = inL;
+		tapeR[writePosition] = inR;
+		floatTapeL[writePosition] = tapeL[writePosition];
+		floatTapeR[writePosition] = tapeR[writePosition];
+		writePosition++;
+		if (writePosition == std::size(tapeL))
+			writePosition = 0;
+		outputs[0][s] = inL;
+		outputs[1][s] = inR;
 	}
 }
 #endif
--- source/Flutterbird.h
+++ source/Flutterbird.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include "IPlug_include_in_plug_hdr.h"
+#include <vector>
+
+const int tapeLengthInSeconds = 10;
 
 using namespace iplug;
 using namespace igraphics;
@@ -11,6 +14,13 @@ public:
 	Flutterbird(const InstanceInfo& info);
 
 #if IPLUG_DSP // All DSP methods and member variables should be within an IPLUG_DSP guard, should you want distributed UI
+	void InitTape();
 	void ProcessBlock(sample** inputs, sample** outputs, int nFrames) override;
+
+	std::vector<double> tapeL;
+	std::vector<double> tapeR;
+	std::vector<float> floatTapeL;
+	std::vector<float> floatTapeR;
+	int writePosition = 0;
 #endif
 };
